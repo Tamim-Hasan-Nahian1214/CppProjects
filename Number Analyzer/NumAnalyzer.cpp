1e42c0207a754c41a96b423d3491f732d0cdd5a3
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-04-06 20:18:51 +0600

    .

--- Number Analyzer/NumAnalyzer.cpp
+++ Number Analyzer/NumAnalyzer.cpp
@@ -7,8 +7,11 @@ int main() {
         int input;
         std::cout << "Enter a number: ";
         std::cin >> input;
+        string sinput = to_string(input);
+        std::cout << sinput << std::endl;
         input % 2 == 0 ? std::cout << "Even Number" << std::endl : std::cout << "Odd Number" << std::endl;
         input < 0 ? std::cout << "Negetive Number" << std::endl : std::cout << "Positive Number" << std::endl;
+        std::cout << sinput.length() << std::endl;
     } 
     return 0;
 }
