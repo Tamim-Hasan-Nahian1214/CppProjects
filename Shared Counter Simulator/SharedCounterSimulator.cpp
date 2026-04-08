e3509f9960dc9eb798c7ab016a886d7490730a24
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-04-08 23:20:19 +0600

    adding a new project

---
+++ Shared Counter Simulator/SharedCounterSimulator.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <memory>
+
+using namespace std;
+
+int main()
+{
+    std::shared_ptr<int> p = std::make_shared<int>(1);
+    std::shared_ptr<int> p2 = p;
+    std::shared_ptr<int> p3 = p2;
+    while (true)
+    {
+        int input;
+        std::cout << "Enter an operation (1, 2, 3, 4): ";
+        std::cin >> input;
+        switch (input)
+        {
+        case 1:
+            *p += 1;
+            std::cout << *p << std::endl;
+        }
+    }
+    return 0;
+}
