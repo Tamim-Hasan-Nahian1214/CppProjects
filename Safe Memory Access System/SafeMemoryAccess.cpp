70619d44f9357b94cb2e293325d8a519f4c008b0
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-04-09 22:59:33 +0600

    .

--- Safe Memory Access System/SafeMemoryAccess.cpp
+++ Safe Memory Access System/SafeMemoryAccess.cpp
@@ -19,6 +19,25 @@ int main()
         else if (wp.expired()) {
             std::cout << "Data expired" << std::endl;
         }
+        int input2;
+        if (input == 2) {
+            std::cout << "Enter new value for the data: ";
+            std::cin >> input2;
+            *sp = input2;
+            std::cout << "Value Changed! enter 1 to check the value" << std::endl;
+        }
+        else if (input == 3) {
+            sp.reset();
+            std::cout << "Data Deleted." << std::endl;
+        }
+        else if (input == 4) {
+            if(!wp.expired()) {
+                std::cout << "Data is Alive." << std::endl;
+            }
+            else {
+                std::cout << "Data Expired." << std::endl;
+            }
+        }
     }
     return 0;
 }
