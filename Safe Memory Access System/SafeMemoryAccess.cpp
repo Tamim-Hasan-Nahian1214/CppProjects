9cea7bd8724d48dbbcc95e59ee532dabb19518fd
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-04-09 20:08:57 +0600

    .

--- Safe Memory Access System/SafeMemoryAccess.cpp
+++ Safe Memory Access System/SafeMemoryAccess.cpp
@@ -7,5 +7,16 @@ int main()
 {
     std::shared_ptr<int> sp = std::make_shared<int>(13);
     weak_ptr<int> wp = sp;
+    while(true) {
+        int input;
+        std::cout << "Enter your input: ";
+        std::cin >> input;
+        if (auto temp = wp.lock()) {
+            std::cout << *temp << std::endl;
+        }
+        else {
+            std::cout << "Data expired" << std::endl;
+        }
+    }
     return 0;
 }
