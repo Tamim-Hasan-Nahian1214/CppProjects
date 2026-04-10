c6ca8a6c5dce945ead7b160164f34c58de8a45b0
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-04-10 11:20:55 +0600

    .

--- Simple Login System/SimpleLogin.cpp
+++ Simple Login System/SimpleLogin.cpp
@@ -4,11 +4,18 @@
 using namespace std;
 
 int main() {
-    std::shared_ptr<string> username = std::make_shared<string>();
-    std::shared_ptr<string> password = std::make_shared<string>();
+    std::shared_ptr<string> username = std::make_shared<string>("Tamim Hasan Nahian");
+    std::shared_ptr<string> password = std::make_shared<string>("SayHellotoBangladesh!");
     string UsernameInput;
     while (true) {
         std::cout << "Enter Your Username: ";
+        getline(cin, UsernameInput);
+        if (UsernameInput == *username) {
+            std::cout << "Correct Username!" << std::endl;
+        }
+        else {
+            std::cout << "Wrong Username!" << std::endl;
+        }
     }
     return 0;
 }
