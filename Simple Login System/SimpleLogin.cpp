1c45df57aa9dbe75cf154f6138e6eed9dd15fd2e
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-04-10 15:06:51 +0600

    /

--- Simple Login System/SimpleLogin.cpp
+++ Simple Login System/SimpleLogin.cpp
@@ -4,20 +4,9 @@
 using namespace std;
 
 int main() {
-    std::shared_ptr<string> username = std::make_shared<string>("Tamim Hasan Nahian");
-    std::shared_ptr<string> password = std::make_shared<string>("SayHellotoBangladesh!");
-    weak_ptr<string> WeakUsername = username;
-    weak_ptr<string> WeakPassword = password;
-    string UsernameInput;
-    string UserPasswordInput;
-    while (true) {
-        std::cout << "Enter Your Username: ";
-        getline(cin, UsernameInput);
-        UsernameInput == *username ? std::cout << "Correct Username!" << std::endl : std::cout << "Wrong Username!" << std::endl;
-        std::cout << "Enter Your Password: ";
-        getline(cin, UserPasswordInput);
-        UserPasswordInput == *password ? std::cout << "Correct Password!" << std::endl : std::cout << "Incorrect Password!" << std::endl;
-        UsernameInput == *username && UserPasswordInput == *password ? std::cout << "Successfully Logged in!" << std::endl : std::cout << "Invalid Username or Password!" << std::endl;
-    }
+    std::shared_ptr<string> username = std::make_shared<string>("Tamim Hasan");
+    std::shared_ptr<int> password = std::make_shared<int>(123456);
+    weak_ptr<string> UsernameObserver = username;
+    weak_ptr<int> PasswordObserver = password;
     return 0;
 }
