b1123a3803ae7e81e8a72131f06e048d687dc22b
Tamim-Hasan-Nahian1214 <261452465+Tamim-Hasan-Nahian1214@users.noreply.github.com>
2026-04-12 23:07:34 +0600

    .

--- Student_Marks_Manager/StudentMarksManager.cpp
+++ Student_Marks_Manager/StudentMarksManager.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
-	std::cout << "Hello World!" << std::endl;
+int main()
+{
+	int LoopTime;
+	std::cout << "Enter How Many Marks You Want to Add: ";
+	std::cin >> LoopTime;
+	vector<int> Marks;
+	int MarksPushInt;
+	for (int i = 1; i < LoopTime + 1; i++)
+	{
+		std::cout << "Enter " << i << "st" << "Mark: ";
+		std::cin >> MarksPushInt;
+		Marks.push_back(MarksPushInt);
+	}
+	for (int Mark : Marks) {
+		std::cout << Mark << std::endl;
+	}
 	return 0;
 }
